ee68add4628bd447a31266140f4f20b205ea4913
yaim0425 <yaim0425@gmail.com>
2025-09-10 06:05:21 -0500

    Se agrega una descripción en la opcion del MOD

--- settings.lua
+++ settings.lua
@@ -44,7 +44,8 @@ table.insert(This_MOD.setting, {
     type = "int",
     name = "time",
     localised_name = { "description.crafting-time" },
-    minimum_value = 1,
+    localised_description = "Min. 1 (1 segundo) \nMax. 65k (18 horas) \nDef. 300 (5 minutos)\n 5min * 60seg = 300seg",
+    minimum_value = 1,     --- 1 segundo
     maximum_value = 65000, --- 18 horas
     default_value = 300    --- 5 minutos
 })
