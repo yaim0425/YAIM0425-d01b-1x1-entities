c93e42928d7bcb3fa0a90e2bf22d30f44b14075d
yaim0425 <yaim0425@gmail.com>
2025-09-13 14:27:11 -0500

    Cambio en la descripción de la opción

--- settings-final-fixes.lua
+++ settings-final-fixes.lua
@@ -44,7 +44,7 @@ table.insert(This_MOD.setting, {
     type = "int",
     name = "time",
     localised_name = { "description.crafting-time" },
-    localised_description = "Min. 1 [ 1seg ] \nMax. 65k [ 18h ] \nDef. 300 [5min]\n 5min * 60seg = 300seg",
+    localised_description = "Min. 1 [ 1seg ] \nMax. 65k [ 18h ] \nDef. 300 [5min]\n\n 5min * 60seg = 300seg",
     minimum_value = 1,     --- 1 segundo
     maximum_value = 65000, --- 18 horas
     default_value = 300    --- 5 minutos
